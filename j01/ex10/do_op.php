95b0110954c2eb694c5acd4ae4d2c52972a955fe
Freccia <Freccia@users.noreply.github.com>
2016-10-05 15:19:58 +0200

    j01/ex10 ++

---
+++ j01/ex10/do_op.php
@@ -0,0 +1,34 @@
+#!/usr/bin/php
+<?PHP
+
+function ft_main($ac, $av)
+{
+	if ($ac != 4)
+		return 0;
+
+	$res = NULL;
+	$n1 = preg_replace("/\s+/", "", $av[1]);
+	$op = preg_replace("/\s+/", "", $av[2]);
+	$n2 = preg_replace("/\s+/", "", $av[3]);
+
+	$n1 = intval($n1);
+	$n2 = intval($n2);
+
+	if ($op == '+')
+		$res = $n1 + $n2;
+	else if ($op == '-')
+		$res = $n1 - $n2;
+	else if ($op == '*')
+		$res = $n1 * $n2;
+	else if ($op == '/')
+		$res = $n1 / $n2;
+	else if ($op == '%')
+		$res = $n1 % $n2;
+
+	if ($res != NULL)
+		echo "$res\n";
+}
+
+ft_main($argc, $argv);
+
+?>
